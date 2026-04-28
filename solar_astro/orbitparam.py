78be660ff741aa32a2e71149b6ac19777c593fde
saika-sei <jayyoo2002@snu.ac.kr>
2026-04-28 17:48:29 +0900

    new obj

--- solar_astro/orbitparam.py
+++ solar_astro/orbitparam.py
@@ -1,6 +1,6 @@
 from astroquery.jplhorizons import Horizons
 
-obj = Horizons(id='Hektor', location='@sun', epochs=2458400.5)
+obj = Horizons(id='469219', location='@sun', epochs=2458400.5)
 el = obj.elements()
 
 a = el['a'][0]        # Semi-major axis (AU)
@@ -10,5 +10,5 @@ w = el['w'][0]        # Argument of perihelion (deg)
 node = el['Omega'][0] # Longitude of ascending node (deg)
 M = el['M'][0]        # Mean anomaly (deg)
 
-print(f" HEKTOR  ep=2458400.5d0")
+print(f" KAMOOALEWA  ep=2458400.5d0")
 print(f" {a:.16f}d0 {e:.16f}d0 {i:.16f}d0 {w:.16f}d0 {node:.16f}d0 {M:.16f}d0 0.d0 0.d0 0.d0")
